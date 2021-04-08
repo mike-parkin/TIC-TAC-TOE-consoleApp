ba1e043bcb3048d0dcf03e5234bdd1a5730c4598
mike-parkin <michael.parkin1988@gmail.com>
2021-04-09 09:58:57 +1200

    player and computer player move functions made

--- player.py
+++ player.py
@@ -15,11 +15,25 @@ class RandomComputerPlayer(Player):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
+        # computer makes a random choice out of the list of availabel moves remaining
+        square = random.choice(game.availablemoves())
+        return square
 
 class HumanPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
     
     def get_move(self, game):
-        pass
+        valid_square = False
+        val = none
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0-9): ')
+            try:
+                # check to see if input is an integer or in the list of available moves
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print('Invalid Square. Try again')
+        return val
