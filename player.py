5ce99bacb7b4a2c0a1d44582fe25004b7feee956
mike-parkin <michael.parkin1988@gmail.com>
2021-04-21 20:53:53 +1200

    add base cases for minimax function

--- player.py
+++ player.py
@@ -50,4 +50,16 @@ def GeniusComputerPlayer(Player):
         return square
 
     def minimax(self, state, player):
-        
+        max_player = self.letter
+        other_player = '0' if player == 'X' else 'X'
+
+        if state.current_winner == other_player:
+            return {
+                'position:': None,
+                'score': 1 * (state.num_empty_squares() + 1) if other_player == max_player else -1 * (state.num_empty_squares() + 1)
+            }
+        elif not state.empty_squares():
+            return {
+                'position': None,
+                'score': 0
+            }
